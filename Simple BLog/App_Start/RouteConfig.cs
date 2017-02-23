dc45e612bd1bab7abbaade573c0e377c13ba4abe
FIDELALKAN\fidel <fidely.alkan@gmail.com>
2017-02-23 18:00:02 +0200

    initial commit

--- Simple BLog/App_Start/RouteConfig.cs
+++ Simple BLog/App_Start/RouteConfig.cs
@@ -12,12 +12,20 @@ namespace Simple_BLog
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
+                    name: "Home",
+                    url: "",
+                    defaults: new { controller = "Posts", action = "Index" }
+                );
+            routes.MapRoute(
+                name: "Login",
+                url: "login",
+                defaults: new { controller = "Auth", action = "Login"});
+           /* routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
---
+++ Simple BLog/Controllers/PostsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_BLog.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("<hr/>View");
+        }
+    }
+}
